70064ceea848586acc9052b352343e20a4686c04
Tyjoo26 <Tyjoo21@gmail.com>
2018-06-06 14:16:17 -0600

    add css styling methods

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import Nav from './components/Nav'
 import Home from './components/Home'
-import { Button } from 'reactstrap';
+
 import './App.css';
 
 class App extends Component {
