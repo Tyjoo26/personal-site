734d70e7ddf15b074334f0aed811dfcee24ccaa2
Tyjoo26 <Tyjoo21@gmail.com>
2018-06-06 14:16:31 -0600

    Add buttons to home page for linked in and github

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import face from '../face.jpg'
+import { Button} from 'reactstrap';
 import '../App.css';
 
+const link = {
+  backgroundColor: '#F16E10',
+  marginRight: '5px'
+}
+
+const git = {
+  backgroundColor: '#F16E10'
+}
 
 class Home extends Component {
   render() {
     return(
       <div className="home-style">
         <img src={face} className="thumb"/>
-        <h2 className="welcome">Timothy Y. Joo </h2>
+        <div className="welcome-content">
+          <h2 className="welcome">Timothy Y. Joo </h2>
+            <Button style={link} bsStyle="primary" bSize="large">LinkedIn</Button>
+            <Button style={git} bsStyle="primary" bSize="large">GitHub</Button>
+        </div>
       </div>
     )
   }
